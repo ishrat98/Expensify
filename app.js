bf9326e59ab938d3377d115e4dafe3e031453ee6
ishrat emu <ishrat.emu@selise.com.bd>
2020-01-16 18:58:26 +0600

    upto input type

--- app.js
+++ app.js
@@ -7,21 +7,49 @@ var budgetController = (function()  {
 //UI Controller
 var UIController = (function()  {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn'
+
+    };
+
+    return {
+        getInput : function()   {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function()   {
+            return DOMstrings;
+        }
+    };
+
 
 })();
 
 //Global App Controller
 var controller = (function(budgetCtrl, UICtrl)  {
 
-    document.querySelector('.add__btn').addEventListener('click', function()    {
+    var DOM= UICtrl.getDOMstrings();
+    var ctrlAddItem = function() {
 
-    });
+        var input = UICtrl.getInput();
+        console.log(input);
 
-    document.addEventListener('keypress', function ()   {
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event)   {  //.....
 
         if (event.keyCode==13 || event.which==13)   {
 
-            console.log('pressed');
+            ctrlAddItem();
 
         }
     });
