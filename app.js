047256e0b86feba584e7a5eb5a9321e4efc6840f
ishrat emu <ishrat.emu@selise.com.bd>
2020-01-19 18:35:37 +0600

    upto html string placeholder

--- app.js
+++ app.js
@@ -78,6 +78,27 @@ var UIController = (function()  {
             };
         },
 
+        addListItem: function(obj, type)    {
+
+        
+            // create html string with placeholder text
+            var html , newHtml;
+            if(type === 'inc') {
+
+                html =  '<div class="item clearfix" id="income-%id"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }   else if(type === 'exp')    {
+                
+                html = '<div class="item clearfix" id="expense-%id"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // replace the placeholder text with the actual data
+            
+            newHtml = html.replace('%id',obj.id);
+         //   newHtml =
+
+            // Insert the HTML into the DOM
+        }
+
         getDOMstrings: function()   {
             return DOMstrings;
         }
